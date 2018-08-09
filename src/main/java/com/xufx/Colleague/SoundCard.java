f76b0ac850fa69f94f0e6505a7ac18c8947ae921
zude <zude@seeapp.com>
2018-08-09 10:14:25 +0800

    第一次提交

---
+++ src/main/java/com/xufx/Colleague/SoundCard.java
@@ -0,0 +1,22 @@
+package com.xufx.Colleague;
+
+import com.xufx.Abstract.Colleague;
+import com.xufx.Interface.Mediator;
+
+/**
+ * 声卡类，一个同事类
+ */
+public class SoundCard extends Colleague {
+    public SoundCard(Mediator mediator) {
+        super(mediator);
+    }
+
+    /**
+     * 按照声频数据发出声音
+     * @param data 发出声音的数据
+     */
+    public void soundData(String data){
+        System.out.println("画外音："+data);
+    }
+
+}
---
+++ src/main/java/com/xufx/Colleague/VideoCard.java
@@ -0,0 +1,21 @@
+package com.xufx.Colleague;
+
+import com.xufx.Abstract.Colleague;
+import com.xufx.Interface.Mediator;
+
+/**
+ * 显卡类，一个同事类
+ */
+public class VideoCard extends Colleague {
+    public VideoCard(Mediator mediator) {
+        super(mediator);
+    }
+
+    /**
+     * 显示视频数据
+     * @param data 被显示的数据
+     */
+    public void showData(String data){
+        System.out.println("您正观看的是："+data);
+    }   
+}
